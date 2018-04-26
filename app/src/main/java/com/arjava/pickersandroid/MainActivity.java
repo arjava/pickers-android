ed74412dc8948a51c449878b1522929b40445fb0
arjava <arjava379@gmail.com>
2018-04-26 21:51:55 +0700

    added TimePickerFragment

--- app/src/main/java/com/arjava/pickersandroid/MainActivity.java
+++ app/src/main/java/com/arjava/pickersandroid/MainActivity.java
@@ -1,12 +1,22 @@
 package com.arjava.pickersandroid;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 import com.arjava.pickersandroid.pickers.DatePickerFragment;
+import com.arjava.pickersandroid.pickers.TimePickerFragment;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -25,14 +35,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
+
+        DialogFragment newFragment;
+
         switch (view.getId()){
             case R.id.btnDatepicker:
-                DialogFragment newFragment = new DatePickerFragment();
+                //cara 1
+                newFragment = new DatePickerFragment();
                 newFragment.show(getSupportFragmentManager(), "DatePicker");
+
+                //cara 2
+                /*Calendar newCalendar = Calendar.getInstance();
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                        Calendar newDate = Calendar.getInstance();
+                        newDate.set(year, monthOfYear, dayOfMonth);
+                        SimpleDateFormat dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+                    }
+
+                },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+                datePickerDialog.show();*/
                 break;
             case R.id.btnTimepicker:
+                //cara 1
+                /*newFragment = new TimePickerFragment();
+                newFragment.show(getSupportFragmentManager(), "TimePicker");*/
 
+                //cara 2
+                Calendar newCalendar = Calendar.getInstance();
+                TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hour, int minute) {
+                        //ketika waktu telah dipilih
+                    }
+                },newCalendar.get(Calendar.HOUR_OF_DAY), newCalendar.get(Calendar.MINUTE), android.text.format.DateFormat.is24HourFormat(this));
+                timePickerDialog.show();
                 break;
         }
     }
+
 }
